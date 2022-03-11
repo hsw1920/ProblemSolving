0d3de34d5fda8c97aa9e53b5fd31f24510b5a7b0
hsw1920 <hsw1920@gmail.com>
2022-03-11 21:56:41 +0900

    .

---
+++ 9251/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  9251
+//
+//  Created by 홍승완 on 2022/03/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
