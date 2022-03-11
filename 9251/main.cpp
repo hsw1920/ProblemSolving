f1cbcfd7f6132943c333dbd69c8080d8393730ac
hsw1920 <hsw1920@gmail.com>
2022-03-11 21:57:05 +0900

    .

--- 9251/main.cpp
+++ 9251/main.cpp
@@ -6,9 +6,24 @@
 //
 
 #include <iostream>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    string a,b;
+    cin>>a>>b;
+    int dp[1001][1001]={0,};
+    for(int i=1;i<=b.size();i++){
+        for(int j=1;j<=a.size();j++){
+            if(a[j-1]==b[i-1])
+                dp[i][j]=dp[i-1][j-1]+1;
+            else
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    
+    cout<<dp[b.size()][a.size()];
+    
     return 0;
 }
