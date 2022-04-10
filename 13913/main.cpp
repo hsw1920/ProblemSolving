e0f78c1a4a62b72c46ab62cb731af4da3f37b5f9
hsw1920 <hsw1920@gmail.com>
2022-04-11 08:49:17 +0900

    .

---
+++ 13913/main.cpp
@@ -0,0 +1,76 @@
+//
+//  main.cpp
+//  13913
+//
+//  Created by 홍승완 on 2022/04/11.
+//
+
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n,k;
+int vis[100001]={0,};
+int route[100001]={0,};
+queue<int>q;
+int t=0,cnt;
+void bfs(){
+    q.push(n);
+    vis[n]=1;
+    int w=1;
+    while (!q.empty()) {
+        cnt=0;
+        for(int i=0;i<w;i++){
+            int now=q.front();
+            q.pop();
+            if(now==k)
+                return;
+            if (now - 1 >= 0 && vis[now - 1] == 0) {
+                q.push(now - 1);
+                vis[now - 1] = 1;
+                route[now - 1] = now;
+                cnt++;
+            }
+            if (now + 1 <= 100000 && vis[now + 1] == 0) {
+                q.push(now + 1);
+                vis[now + 1] = 1;
+                route[now + 1] = now;
+                cnt++;
+            }
+            if (now * 2 <= 100000 && vis[now * 2] == 0) {
+                q.push(now * 2);
+                vis[now * 2] = 1;
+                route[now * 2] = now;
+                cnt++;
+            }
+        }
+        w=cnt;
+        if(cnt!=0)
+            t++;
+    }
+}
+int main() {
+    //n, k, 10만
+    // x+1, x-1, 2*x로 이동
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>k;
+    bfs();
+    cout<<t<<"\n";
+    int now=k;
+    int prev;
+    stack<int>st;
+    while(now!=n){
+        st.push(now);
+        prev=route[now];
+        now=prev;
+    }
+    cout<<n<<" ";
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    return 0;
+}
