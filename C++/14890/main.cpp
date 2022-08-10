7cfd96aee423e417a1a7b21d5af6fd41aadfcfac
hsw1920 <hsw1920@gmail.com>
2022-08-10 16:24:27 +0900

    20220810

---
+++ C++/14890/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  14890
+//
+//  Created by 홍승완 on 2022/08/01.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    
+    
+    return 0;
+}
