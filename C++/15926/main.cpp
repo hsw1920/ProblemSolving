592823fa7838fad12cba1de2215df3849aa413e0
hsw1920 <hsw1920@gmail.com>
2022-08-27 18:53:05 +0900

    20220827

--- C++/15926/main.cpp
+++ C++/15926/main.cpp
@@ -12,11 +12,31 @@
 using namespace std;
 
 int n;
+string str;
+stack<int> st;
+int res,cnt;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-
+    cin>>n;
+    cin>>str;
+    st.push(-1);
+    for(int i=0;i<n;i++){
+        if(str[i]=='(')
+            st.push(i);
+        
+        if(str[i]==')') {
+            st.pop();
+            if(st.size()){
+                res = max(res, i-st.top());
+            }
+            else {
+                st.push(i);
+            }
+        }
+    }
+    cout<<res<<"\n";
+  
     return 0;
 }
---
+++ C++/3015/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  3015
+//
+//  Created by 홍승완 on 2022/08/27.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    7
+    
+    1 5 2 2 1 4 2
+      1 1 2 3 4 
+    return 0;
+}
