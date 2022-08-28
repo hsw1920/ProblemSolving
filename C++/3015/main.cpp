8e9c9969340573a8bce2a74cd2b725e63642208a
hsw1920 <hsw1920@gmail.com>
2022-08-28 12:56:43 +0900

    20220828

--- C++/3015/main.cpp
+++ C++/3015/main.cpp
@@ -16,9 +16,30 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    7
     
-    1 5 2 2 1 4 2
-      1 1 2 3 4 
+    long long n, res,tmp;
+    stack<pair<long long, long long>>st;
+    cin>>n;
+    res=0;
+    for(int i=0;i<n;i++){
+        cin>>tmp;
+        int cnt=1;
+        while(st.size()&& st.top().first<=tmp){
+            res+=st.top().second;
+            if(st.top().first==tmp){
+                cnt=st.top().second +1;
+            }
+            else{
+                cnt=1;
+            }
+            st.pop();
+        }
+        if(st.size()){
+            res++;
+        }
+        st.push({tmp,cnt});
+    }
+    cout<<res<<"\n";
+    
     return 0;
 }
