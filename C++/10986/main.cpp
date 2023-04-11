f9248f693cafdf9d79f4e39afea268ccb48036b2
hsw1920 <hsw1920@gmail.com>
2023-04-11 13:08:12 +0900

    20230411

---
+++ C++/10986/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  10986
+//
+//  Created by 홍승완 on 2023/04/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
