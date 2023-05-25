87c4e7879def214e42ff0019f8e196f76e1c9b6b
hsw1920 <hsw1920@gmail.com>
2023-05-25 23:36:34 +0900

    20230525

---
+++ C++/2176/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  2176
+//
+//  Created by 홍승완 on 2023/05/24.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int n,m,u,v,w;
+typedef pair<int,int> pint;
+vector<pint>edge[1001];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    
+    
+    
+    return 0;
+}
