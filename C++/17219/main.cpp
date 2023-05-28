0f46e06d43bc79f20103fb8fc706b5fa550c4972
hsw1920 <hsw1920@gmail.com>
2023-05-28 10:09:48 +0900

    20230528

---
+++ C++/17219/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  17219
+//
+//  Created by 홍승완 on 2023/05/28.
+//
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+    int n, m;
+    string id, pw;
+    map <string, string> account;
+    cin >> n >> m;
+    for(int i=0; i<n; i++) {
+        cin >> id >> pw;
+        account[id] = pw;
+    }
+    for(int i=0; i<m; i++) {
+        cin >> id;
+        cout << account[id] << '\n';
+    }
+}
