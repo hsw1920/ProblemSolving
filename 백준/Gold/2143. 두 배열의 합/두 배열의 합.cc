45cd379ec68ad7f74012c2df4231bfe585e71220
seuhong <66902876+hsw1920@users.noreply.github.com>
2024-01-01 15:09:17 +0900

    [Gold III] Title: 두 배열의 합, Time: 100 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -11,48 +11,48 @@
 
 using namespace std;
 
-int t,n,m;
-vector<int>asum,bsum;
-int a[1001];
-int b[1001];
+long long T,n,m;
+long long A[1001],B[1001];
+long long psum1[1001],psum2[1001];
+vector<long long>v1,v2;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     
-    cin>>t;
+    cin>>T;
     cin>>n;
-    int tmp;
-    for(int i=0;i<n;i++){
-        cin>>a[i];
+    for(int i=1;i<=n;i++){
+        cin>>A[i];
+        psum1[i]=psum1[i-1]+A[i];
     }
     cin>>m;
-    for(int i=0;i<m;i++){
-        cin>>b[i];
+    for(int i=1;i<=m;i++){
+        cin>>B[i];
+        psum2[i]=psum2[i-1]+B[i];
     }
-    for(int i=0;i<n;i++){
-        int sum=a[i];
-        asum.push_back(sum);
-        for(int j=i+1;j<n;j++){
-            sum+=a[j];
-            asum.push_back(sum);
+    
+    //A와 B의 부 배열의 합이 T가 되는 모든 쌍의 개수를 구해라
+    // 각 부배열이 될 수 있는 집합을 모두 구한다?
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=n;j++){
+            v1.push_back(psum1[j]-psum1[i-1]);
         }
     }
-    for(int i=0;i<m;i++){
-        int sum=b[i];
-        bsum.push_back(sum);
-        for(int j=i+1;j<m;j++){
-            sum+=b[j];
-            bsum.push_back(sum);
+    for(int i=1;i<=m;i++){
+        for(int j=i;j<=m;j++){
+            v2.push_back(psum2[j]-psum2[i-1]);
         }
     }
-    sort(bsum.begin(),bsum.end());
+    sort(v1.begin(),v1.end());
+    sort(v2.begin(),v2.end());
+    
     long long res=0;
-    for(int i=0;i<asum.size();i++){
-        int target=t-asum[i];
-        int lb=lower_bound(bsum.begin(), bsum.end(), target)-bsum.begin();
-        int ub=upper_bound(bsum.begin(), bsum.end(), target)-bsum.begin();
-        res+=(ub-lb);
+    for(int i=0;i<v1.size();i++){
+        long long subA=v1[i];
+        long long target = T-subA;
+        res += upper_bound(v2.begin(), v2.end(), target) - lower_bound(v2.begin(), v2.end(), target);
     }
+    
     cout<<res<<"\n";
     
     return 0;
