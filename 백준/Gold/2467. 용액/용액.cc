3bd38b9f13dd0e828b7ab3a148831859a835b339
seuhong <66902876+hsw1920@users.noreply.github.com>
2024-03-10 11:06:37 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,33 @@
+#include <iostream> 
+#include <vector> 
+#include <algorithm> 
+using namespace std; 
+int main(void) { 
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); int N; 
+    cin >> N; 
+    vector<int> v(N); 
+    for (int i = 0; i < N; i++) { 
+        cin >> v[i]; 
+    } 
+    sort(v.begin(), v.end()); 
+    int i = 0, j = v.size() - 1; 
+    int total = 2e9; 
+    pair<int, int> result; 
+    while (i < j) { 
+        int a = v[i]; 
+        int b = v[j]; 
+        if (abs(a + b) < total) { 
+            total = abs(a + b); 
+            result.first = v[i]; 
+            result.second = v[j]; 
+        } 
+        if (a + b < 0) { 
+            i++; } 
+        else { 
+            j--; 
+             } 
+    } 
+    cout << result.first << " " << result.second << "\n"; 
+    return 0; 
+}
