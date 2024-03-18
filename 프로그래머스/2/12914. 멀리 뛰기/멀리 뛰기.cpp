9792a3feea037e2b73db227a4a0b5e1b5c558098
seuhong <66902876+hsw1920@users.noreply.github.com>
2024-03-18 20:01:27 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    long long dp[2001]={0,};
+    dp[1]=1;
+    dp[2]=2;
+    for(int i=3;i<=n;i++){
+        dp[i]=dp[i-1]%1234567+dp[i-2]%1234567;
+    }
+    answer = dp[n]%1234567;
+    return answer;
+}
