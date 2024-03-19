702864abb18aa851f19e232a3102f96ca485b6c7
seuhong <66902876+hsw1920@users.noreply.github.com>
2024-03-19 13:00:40 +0900

    [level 2] Title: 피로도, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // 최소필요피로도, 소모피로도
 // 현재피로도 k
 // 유저 탐험가능 최대 던전수
-// dp-> 현재피로도, 던전갯수
+// dp-> 현재피로도, 던전갯수 ㄴㄴ
 typedef pair<int,int> pint;
 int solution(int k, vector<vector<int>> dungeons) {
     int answer = 0;
@@ -22,7 +22,6 @@ int solution(int k, vector<vector<int>> dungeons) {
         }    
         answer=max(ans,answer);
     } while(next_permutation(dungeons.begin(),dungeons.end()));
-    
     
     return answer;
 }
