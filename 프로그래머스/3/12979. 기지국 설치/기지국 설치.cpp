d0884f71dfeb9d2863e5f9dc998e56397dbd8389
seuhong <66902876+hsw1920@users.noreply.github.com>
2024-03-21 15:31:22 +0900

    [level 3] Title: 기지국 설치, Time: 0.17 ms, Memory: 3.93 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -1,31 +1,20 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-// N 2억
-// stations 1만, 오름차순, N보다 같거나 작은 자연수
-// w 1만
-
-// 기지국 교체 -> 모든 아파트에 전파하는 기지국 최소개수
-// W는 양옆 기지국 전파범위
-// 2만*1만 복잡도 가능
-// n은 아파트 개수
 
 int solution(int n, vector<int> stations, int w)
 {
     int answer = 0;
     vector<pair<int,int>>v;
-    // init 시작점 잡기.
     int ped=0;
     for(int i=0;i<stations.size();i++){
-        int cst = stations[i]-w; // 3
-        int ced = stations[i]+w; // 5
+        int cst = stations[i]-w; 
+        int ced = stations[i]+w; 
 
         if(ped+1<cst){
             v.push_back({ped+1,cst-1});
         } 
-        
         ped = ced;
-        
         if(i==stations.size()-1 && ced < n){
             v.push_back({ced+1,n});
         }
@@ -36,7 +25,6 @@ int solution(int n, vector<int> stations, int w)
         int width = 2*w+1;
         answer += (len+width-1)/width;
     }
-    
 
     return answer;
 }
