a747d43ce0c31402019e742e33d41cbad35c921f
seuhong <66902876+hsw1920@users.noreply.github.com>
2024-04-02 16:43:04 +0900

    [level 1] Title: 과일 장수, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.swift
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.swift
@@ -1,13 +1,30 @@
 import Foundation
 
 func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
-    var v = score.sorted(by:>)
-    var cnt = v.count / m
-    var idx = m-1
+    // 그냥정렬 -> O(N*logN)
+    // var v = score.sorted(by:>)
+    // var cnt = v.count / m
+    // var idx = m-1
+    // var res = 0
+    // for i in 0..<cnt {
+    //     res += (v[idx]*m)
+    //     idx += m
+    // }
+    // 계수정렬 -> O(N)
+    var dict = Array(repeating: 0, count: k+1)
+    score.forEach{dict[$0]+=1}
+    dict = [Int](dict.reversed())
+    dict.removeLast()
+    var cnt = 0
     var res = 0
-    for i in 0..<cnt {
-        res += (v[idx]*m)
-        idx += m
+    var cur = k
+    for i in dict {
+        cnt += i
+        while(cnt >= m) {
+            cnt-=m
+            res+=(cur*m)
+        }
+        cur-=1
     }
     
     return res
