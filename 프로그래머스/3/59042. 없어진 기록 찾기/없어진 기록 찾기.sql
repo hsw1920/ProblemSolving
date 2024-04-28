6e3c96698a1538022ad20ca51177c32db3a5c422
seuhong <66902876+hsw1920@users.noreply.github.com>
2024-04-28 11:48:41 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,15 +1,5 @@
-select A.animal_id, A.name
-from animal_outs as A
-join
-    (
-    select outs.animal_id, ins.name
-    from animal_outs as outs
-    left join animal_ins as ins
-    on outs.animal_id = ins.animal_id
-    where outs.name is not null
-    ) as B
-on A.animal_id = B.animal_id and B.name is null
-order by animal_id
-    
-    
-# select * from animal_outs
+select B.animal_id, B.name
+from animal_ins A
+right join animal_outs B
+on A.animal_id = B.animal_id
+where A.animal_id is null
